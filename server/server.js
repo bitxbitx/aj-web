0cac0b100d4708505079639730979e7034458ca6
Stanley Ng <sngg0055@student.monash.edu>
2023-02-03 11:23:02 +0800

    Init Files

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors')
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db')
+const cookieParser = require('cookie-parser')
+const morgan = require("morgan");
+
+const port = process.env.PORT || 8000
+
+connectDB()
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://localhost:3000', 'http://localhost:8000']
+}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/customers', require('./routes/customerRoutes'))
+app.use('/api/admins', require('./routes/adminRoutes'))
+app.use('/api/products', require('./routes/productRoutes'))
+app.use('/api/auth/admin', require('./routes/auth/authAdminRoutes'))
+app.use('/api/auth/customer', require('./routes/auth/authCustomerRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
